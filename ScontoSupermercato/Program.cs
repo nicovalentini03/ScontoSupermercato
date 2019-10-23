a600640bb1def8c81b909a459c7b7c2e3fe8f1e2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 12:18:05 +0200

    Add project files.

---
+++ ScontoSupermercato/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScontoSupermercato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
